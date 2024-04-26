63b0224dee36550f00daa6a509cfedfd8bc9156d
Bernhard Kapelari – kapelari.studio <bernhard@kapelari.com>
2024-04-26 07:25:44 +0200

    funktioniert als theme

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-        <footer>
+        <footer class="bg-dark text-light py-5">
             <div class="container">
                 <div class="row">
-                    
+                    ich bin der footer
                 </div>
             </div>
         </footer>
